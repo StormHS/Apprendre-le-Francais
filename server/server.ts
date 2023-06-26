849fed742ec4584e5c8b94ab12d021fddc4c48d1
Storm Harris-Studdart <103412904+StormHS@users.noreply.github.com>
2023-06-26 14:52:50 +1200

    dokku fixups

--- server/server.ts
+++ server/server.ts
@@ -6,13 +6,13 @@ import translationRoutes from './routes/translations'
 const server = express()
 
 server.use(express.json())
+server.use('/api/v1/translations', translationRoutes)
+
 if (process.env.NODE_ENV === 'production') {
   server.use('/assets', express.static(path.resolve(__dirname, '../assets')))
   server.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../index.html'))
   })
 }
-
-server.use('/api/v1/translations', translationRoutes)
 
 export default server
